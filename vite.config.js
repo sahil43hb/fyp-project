b6515e119fdf9c939af4bdcd3076349d7139648b
sahil43hb <sahilmubeen30@gmail.com>
2024-12-06 01:17:19 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                "resources/css/app.css",
+                // "resources/css/app.css",
                 "resources/js/app.js",
                 "resources/js/userForm.js",
                 "resources/js/admin/main.js",
